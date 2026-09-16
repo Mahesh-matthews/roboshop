62d8697ddeb145d9c244bb7968f3031d516edaaf
Mahesh Kasapu <mahimystu@gmail.com>
2026-09-16 21:38:39 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ for instance in $@
 do 
 
     INSTANCE_ID=$( aws ec2 run-instances \
-    --image-id @AMI_ID \
+    --image-id $AMI_ID \
     --instance-type t3.micro \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
